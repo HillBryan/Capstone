3f4b518b9fa115e9802fe659dfbd69df9ee16405
HillBryan <hillbj1@appstate.edu>
2022-02-21 17:56:38 -0500

    Started Home Page.

--- capstone/src/components/nav-bar.vue
+++ capstone/src/components/nav-bar.vue
@@ -1,24 +1,34 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Capstone Project</a>
+    <router-link class="navbar-brand" to="/">Capstone Project</router-link>
     <button
       class="navbar-toggler"
       type="button"
       data-toggle="collapse"
-      data-target="#navbarNavAltMarkup"
-      aria-controls="#navbarNavAltMarkup"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
       aria-expanded="false"
       aria-label="Toggle navigation"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link class="nav-link nav-item" to="/">Home</router-link>
-        <router-link class="nav-link nav-item" to="/about">About</router-link>
-        <router-link class="nav-link nav-item" to="/classes">Classes</router-link>
-        <router-link class="nav-link nav-item right" to="/login">Login</router-link>
-      </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/classes">Classes</router-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login">Login</router-link>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -29,10 +39,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 
-.right {
-  justify-content: fl;
+.nav-item {
+  text-align: center;
 }
 
 </style>
